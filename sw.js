8e404eace7337d9b12e66171200a8f867c247b5d
marianepsantos <marianepsantos15@gmail.com>
2024-11-14 17:35:54 -0400

    Commit
    Commit

--- sw.js
+++ sw.js
@@ -1,23 +1,30 @@
-let cacheName = "my-first-pwa";
-let filesToCache = ["/", "/index.html", 
-                "/css/style.css", "/js/main.js"];
+let cacheName = "receitas-receitas-pwa";
+let filesToCache = ["/",
+    "/index.html",
+    "/carness",
+    "/docess",
+    "/massass",
+    "/css/style.css",
+    "/js/main.js",
+    "/manifest.json"  
+];
 
-/* inicializando a service worker e fazendo o 
-download do conteúdo da aplicação */
+
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
-      return cache.addAll(filesToCache);
+      console.log("Service Worker: Arquivos em cache");
+      return cache.addAll(filesToCache);  
     })
   );
 });
 
-/* disponibilizando o conteudo quando estiver offline */
+
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
+      
       return response || fetch(e.request);
     })
   );
 });
-
